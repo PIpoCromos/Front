d204f468197d78bb89b7bbbb3a64cb7130bb5a94
PipoCromos <pipiaguinaga3@gmail.com>
2025-09-24 10:14:19 -0300

    checkbox

--- Codigo-Azul/personal.js
+++ Codigo-Azul/personal.js
@@ -2,3 +2,15 @@ document.querySelector('.alarm-btn').addEventListener('click', function(e) {
     e.preventDefault();
     alert('¡Alerta activada! El personal ha sido notificado.');
 });
+
+ document.querySelectorAll("input[type=checkbox]").forEach(checkbox => {
+      
+      const saved = localStorage.getItem("checkbox-" + checkbox.id);
+      if (saved === "true") {
+        checkbox.checked = true;
+      }
+
+      checkbox.addEventListener("change", () => {
+        localStorage.setItem("checkbox-" + checkbox.id, checkbox.checked);
+      });
+    });
