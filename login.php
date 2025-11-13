ea94950042d16710a82ade4217e5936cb279a877
Benicio Albrecht <benicioalbre@gmail.com>
2025-11-13 15:02:50 -0300

    13/11 -2

--- login.php
+++ login.php
@@ -13,28 +13,32 @@ require_once 'includes/login_view.inc.php';
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h2>Iniciar Sesión</h2>
-    <form action="includes/login.inc.php" method = 'post'>
-    <input type="text" name="email" placeholder="Email">
-    <input type="password" name="pwd" placeholder="Contraseña">
-    <button>Login</button>
+
+    <div class="formulario">
+        <h2>Iniciar sesion</h2>
+        <form action="includes/login.inc.php" method='post'>
+            <div class="regisForm">
+                <input type="email" name="email" id="email" placeholder="Ingrese su email">
+                <input type="password" name="pwd" id="password" placeholder="Ingrese su contraseña">
+                <button class="boton">Login</button>
+            </div>
+        </form>
+    </div>
 
 <?php 
     check_login_errors();
 ?>
 
-    </form action="includes/signup.inc.php">
-
+    <div class="formulario">
+        <h2>Crear cuenta</h2>
+        <form action="includes/signup.inc.php">
             <div class="regisForm">
-                <h2>Registrarse</h2>
-                <!-- <label for="email">Correo Electrónico:</label> -->
                 <input type="email" name="email" id="email" placeholder="Ingrese su email">
-            </div>
-            <div class="regisForm">
-                <!-- <label for="password">Contraseña:</label> -->
                 <input type="password" name="pwd" id="password" placeholder="Ingrese su contraseña">
+                <button class="boton">Registrarse</button>
             </div>
-            <button id="loginBtn">Registrarse</button>
+        </form>
+    </div>
     <script src="login.js"></script>
 </body>
 </html>
